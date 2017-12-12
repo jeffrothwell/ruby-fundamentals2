842782060b63c673d87f067bd75d7b4e8e83d624
jeffrothwell <rothwelljeff@gmail.com>
2017-12-12 12:51:19 -0500

    complete exercise 1

--- exercise1.rb
+++ exercise1.rb
@@ -1,4 +1,28 @@
+#my recommendations
 documentary = "Merchants of Doubt"
 drama = "Pulp Fiction"
 dramedy = "The Life Aquatic"
 comedy = "Pineapple Express"
+
+puts "I'm going to recommend a film for you"
+
+puts "1. Do you like documentaries? (y / n)"
+likedocs = gets.chomp
+
+puts "1. Do you like dramas? (y / n)"
+likedramas = gets.chomp
+
+puts "1. Do you like comedies? (y / n)"
+likecoms = gets.chomp
+
+if likedocs == "y" || likedocs == "yes"
+  puts "I recommend you check out #{documentary}"
+elsif (likedocs == "n" || likedocs == "no") && (likedramas == "y" || likedramas == "yes") && (likecoms == "y" || likecoms == "yes")
+  puts "I recommend you check out #{dramedy}"
+elsif (likedocs == "n" || likedocs == "no") && (likedramas == "y" || likedramas == "yes") && (likecoms == "n" || likecoms == "no")
+  puts "I recommend you check out #{drama}"
+elsif (likedocs == "n" || likedocs == "no") && (likedramas == "n" || likedramas == "no") && (likecoms == "y" || likecoms == "yes")
+  puts "I recommend you check out #{comedy}"
+else
+  puts "Maybe a good book is more your thing\n might I suggest a book about film appreciation?"
+end
